b86b63379f01d58231ba772c55e6e6e78a98856f
ZhangXuenan <zxnnet@gmail.com>
2019-10-01 18:13:05 +0800

    change into my conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    plugins: [
-        {
+    plugins: [{
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/viewvlab/c0ca2218634749be915ed0ce65bcce8d?v=61fc7838810348c88c649f6a39947bf4"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/viewvlab/c6047aa4e9914e2f84b91d0267fa22ea?v=1aa8576e883a4752a46db73d37e09fc3"
             }
         }
     ],
